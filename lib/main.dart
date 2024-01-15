4f95ff4a1e4fc8bd8ab2026b397d95e6b2cdce89
limseongsu <tjdtn2323@naver.com>
2024-01-15 09:21:11 +0900

    fix: 1: const 추가

--- lib/main.dart
+++ lib/main.dart
@@ -23,7 +23,7 @@ class MyApp extends StatelessWidget {
       ),
       home: ChangeNotifierProvider(
         create: (_) => LoginViewModel(),
-        child: LoginScreen(),
+        child: const LoginScreen(),
       ),
     );
   }
